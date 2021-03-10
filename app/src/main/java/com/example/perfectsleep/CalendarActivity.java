5b9bebf8e7888410680513787b56b507e17fb796
alexthatguyfromthegroup <70351674+alexthatguyfromthegroup@users.noreply.github.com>
2021-03-09 23:34:09 -0500

    layouts

--- app/src/main/java/com/example/perfectsleep/CalendarActivity.java
+++ app/src/main/java/com/example/perfectsleep/CalendarActivity.java
@@ -2,34 +2,44 @@ package com.example.perfectsleep;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
+public class CalendarActivity extends AppCompatActivity {
 
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calendar);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button calendarButton = (Button)findViewById(R.id.buttonCalendar);
-        calendarButton.setOnClickListener(new View.OnClickListener() {
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/"+ dayOfMonth ;
+                //Intent intent = new Intent(CalendarActivity.this,MainActivity.class);
+                //intent.putExtra("date",date);
+                //startActivity(intent);
+                Snackbar snack = Snackbar.make(findViewById(R.id.calendarlayout), "Date: " + date + "\nScore: no data" , Snackbar.LENGTH_LONG);
+                snack.show();
+            }
+        });
+        Button backButton = (Button)findViewById(R.id.buttonBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CalendarActivity.class));
+                startActivity(new Intent(CalendarActivity.this, MainActivity.class));
             }
         });
     }
@@ -55,8 +65,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
-
 }
