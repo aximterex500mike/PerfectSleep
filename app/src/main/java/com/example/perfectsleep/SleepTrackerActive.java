34d6a1b886221e1533afc7796be8532c14963bac
alexthatguyfromthegroup <70351674+alexthatguyfromthegroup@users.noreply.github.com>
2021-04-21 20:22:11 -0400

    serviceandapi
    Update includes the google api for detecting sleep, and the service it calls to run in the background

--- app/src/main/java/com/example/perfectsleep/SleepTrackerActive.java
+++ app/src/main/java/com/example/perfectsleep/SleepTrackerActive.java
@@ -11,6 +11,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,59 +34,72 @@ public class SleepTrackerActive extends AppCompatActivity{
 
     private Chronometer chronometer;
     boolean start = true;
-    private int starttime;
-    private int endtime;
+    private long starttime;
+    private long endtime;
+    String id = "test";
     ArrayList<Integer> times = new ArrayList<>();
     ArrayList<Integer> scores = new ArrayList<>();
-    ActivityRecognitionClient actrec;
-    Snackbar snack;
+    ActivityRecognitionClient actrec = null;
     ActivityResultLauncher arl;
+    Intent intent;
+    SharedPreferences sharedpreferences;
+    PendingIntent getData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("SleepTracker", "line 22");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sleep_tracker_active);
-        arl =  registerForActivityResult(new ActivityResultContracts.RequestPermission(), yes -> {});
         //this needed?
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_sleep_tracker_active);
+        arl =  registerForActivityResult(new ActivityResultContracts.RequestPermission(), yes -> {});
 
-        //Start chronometer
-        chronometer = (Chronometer) findViewById(R.id.chronometer);
-        snack = Snackbar.make(findViewById(R.id.sleeplayout), "test popup", Snackbar.LENGTH_LONG);
         //Button code to end sleep tracker and bring the user to the main screen
         Button endSleep = (Button)findViewById(R.id.buttonEndSleepTracker);
         endSleep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!start) {
-                    endtime = 0; //// get end time from phone
-                    //put starttime,endtime, arrays in database here
                     startActivity(new Intent(SleepTrackerActive.this, MainActivity.class));
                 }else{
                     if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED) {
                         arl.launch(Manifest.permission.ACTIVITY_RECOGNITION);
                     }
                     if(ContextCompat.checkSelfPermission(SleepTrackerActive.this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
-                        snack.show();
+
                         chronometer.start();
                         endSleep.setText("End Sleep");
-                        starttime = 0;//////////get start time from phone
+
+                        //this must be stored
                         start = false;
-                        actrec = new ActivityRecognitionClient(SleepTrackerActive.this);
 
-                        //After this is what should be in background if possible
-                        //actrec.requestSleepSegmentUpdates(getData, new SleepSegmentRequest(SleepSegmentRequest.CLASSIFY_EVENTS_ONLY));
+                        SharedPreferences.Editor e = sharedpreferences.edit();
+                        e.putBoolean("button", false);
+                        e.commit();
 
+                        startCollectingData();
                     }
                 }
             }
         });
-
-        Log.d("SleepTracker", "line 41");
     }
-    //PendingIntent getData = PendingIntent.getActivity(SleepTrackerActive.this,1,)
+
+    public void startCollectingData(){
+        starttime = 0;//////////get start time from phone
+        actrec = new ActivityRecognitionClient(SleepTrackerActive.this);
+        intent = new Intent(getApplicationContext(), getSleepData.class);
+        intent.putExtra("id", id);
+        intent.putExtra("start", starttime);
+        getData = PendingIntent.getService(SleepTrackerActive.this,1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        actrec.requestSleepSegmentUpdates(getData, new SleepSegmentRequest(SleepSegmentRequest.CLASSIFY_EVENTS_ONLY));
+    }
+
+    public void endCollectingData(){
+        endtime = 0; //// get end time from phone, store in db
+        actrec.removeSleepSegmentUpdates(getData);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -111,13 +125,21 @@ public class SleepTrackerActive extends AppCompatActivity{
     @Override
     protected void onResume() {
         super.onResume();
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        sharedpreferences = getSharedPreferences("buttonSetting", SleepTrackerActive.this.MODE_PRIVATE);
+        start = sharedpreferences.getBoolean("button", true);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        SharedPreferences.Editor e = sharedpreferences.edit();
+        e.putBoolean("button", true);
+        e.commit();
+        if(actrec !=null) {
+            endCollectingData();
+        }
     }
-
 }
 
 
@@ -125,5 +147,4 @@ public class SleepTrackerActive extends AppCompatActivity{
 /*
 links for future features
 https://stackoverflow.com/questions/7197798/get-the-microphone-sound-level-decibel-level-in-android
-
 */
---
+++ app/src/main/java/com/example/perfectsleep/getSleepData.java
@@ -0,0 +1,41 @@
+package com.example.perfectsleep;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.location.SleepClassifyEvent;
+
+import java.util.List;
+
+public class getSleepData extends IntentService {
+
+
+    public getSleepData() {
+        super("getSleepData");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            if(SleepClassifyEvent.hasEvents(intent)){
+                intent.getLongExtra("starttime", -1); //start time of current sleep
+                intent.getStringExtra("id"); //userid
+                List<SleepClassifyEvent> data = SleepClassifyEvent.extractEvents(intent);
+                for(int i = 0; i < data.size(); i++){
+                    SleepClassifyEvent event = data.get(i);
+                    event.getConfidence(); //1-100, high is better sleep
+                    event.getTimestampMillis(); //time in miliseconds since 1/1/1970
+                    event.getMotion(); //amount of motion from 1 to 6, higher being worse ( i think)
+                    
+                    //
+                    // insert into database here, id should be unique to each person, starttime can identify whats data us in the same night
+                    //
+                }
+                //destroy service after done
+                stopSelf();
+            }
+        }
+    }
+}
